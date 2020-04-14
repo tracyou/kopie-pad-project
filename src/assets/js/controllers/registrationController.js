e467e4dff32f1aae8ade46355f18c51b655c0196
owusuts <tracy.owusu@hva.nl>
2020-04-15 01:11:04 +0200

    Nog een poging verbetering van registratie

--- src/assets/js/controllers/registrationController.js
+++ src/assets/js/controllers/registrationController.js
@@ -9,5 +9,39 @@
 // });
 
 class registrationController {
+    constructor(){
 
+        $.get("views/registration.html")
+            .done((data) => this.setup(data))
+            .fail(() => this.error());
+    }
+
+    setup(data) {
+        this.registrationView = $(data);
+
+        this.registrationView.find("#registration")
+            // .("");
+
+        $(".content").empty().append(this.registrationView);
+    }
+
+
+
+        // function registration() {
+        //     const name = $("#name").val();
+        //     const password1 = $("#password1").val();
+        //     const password2 = $("#password2").val();
+        //
+        //     if (password1 !== password2){
+        //         alert('De wachtwoorden die u heeft ingevuld komen niet overeen!');
+        //     }else if (name.length == 0 || password1.length == 0 || password2.length == 0){
+        //         alert('U heeft niet alle velden ingevuld!');
+        //     }else {
+        //         con.connect(function(err) {
+        //             if (err) throw err;
+        //             const sql = "INSERT INTO user (username, password) VALUES (name,password1)";
+        //             console.log("inserted");
+        //         })
+        //     }
+        // }
 }
