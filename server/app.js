46baede63cba785345864df330d57c4f98d56443
TM van Breukelen <tessa.van.breukelen@hva.nl>
2020-06-01 11:07:55 +0200

    Foutje in de app.js

--- server/app.js
+++ server/app.js
@@ -63,70 +63,66 @@ app.post("/room_example", (req, res) => {
 });
 
 app.post("/user/registration", (req, res) => {
-        const username = req.body.username;
-        const password = cryptoHelper.getHashedPassword(req.body.password);
+    const username = req.body.username;
+    const password = cryptoHelper.getHashedPassword(req.body.password);
 
-        db.handleQuery(connectionPool, {
-            query: "INSERT INTO user (username, password) VALUES (?,?)",
-            values: [username, password]
+    db.handleQuery(connectionPool, {
+        query: "INSERT INTO user (username, password) VALUES (?,?)",
+        values: [username, password]
+    }, (data) => {
+        if (data.length === 1) {
+            //return just the username for now, never send password back!
+            // res.status(httpOkCode).json({"username": data[0].username});
+            res.status(httpOkCode).json(data);
+            console.log("Regitration went wright")
+        } else {
+            //wrong username
+            res.status(authorizationErrCode).json({reason: "Regitration went wrong"});
+        }
+
+    }, (err) => res.status(badRequestCode).json({reason: err}));
+});
+
+app.post("/contactAdd", (req, res) => {
+    const contactName = req.body.contactName;
+    const contactResidence = req.body.contactResidence;
+    const contactDescription = req.body.contactDescription;
+    const contactPhoneNumber = req.body.contactPhoneNumber;
+    const contactQualityMedical = req.body.contactQualityMedical;
+    const contactQualityComputer = req.body.contactQualityComputer;
+    const contactQualitySocial = req.body.contactQualitySocial;
+    const contactQualityDriver = req.body.contactQualityDriver;
+
+    db.handleQuery(connectionPool, {
+            query: "INSERT INTO contact (Name, Residence, TelephoneNr, canDrive, canMeet, Medical, Computer, Description) VALUES (?,?,?,?,?,?,?,?)",
+            values: [contactName, contactResidence, contactPhoneNumber, contactQualityDriver, contactQualitySocial, contactQualityMedical, contactQualityComputer, contactDescription]
         }, (data) => {
-            if (data.length === 1) {
-                //return just the username for now, never send password back!
-                // res.status(httpOkCode).json({"username": data[0].username});
-                res.status(httpOkCode).json(data);
-                console.log("Regitration went wright")
-            } else {
-                //wrong username
-                res.status(authorizationErrCode).json({reason: "Regitration went wrong"});
-            }
+            //just give all data back as json
+            res.status(httpOkCode).json(data);
+        }, (err) => res.status(badRequestCode).json({reason: err})
+    );
+});
 
-            db.handleQuery(connectionPool)
-        });
+app.post("/contactChange", (req, res) => {
+    const contactName = req.body.contactName;
+    const contactResidence = req.body.contactResidence;
+    const contactDescription = req.body.contactDescription;
+    const contactPhoneNumber = req.body.contactPhoneNumber;
+    const contactQualityMedical = req.body.contactQualityMedical;
+    const contactQualityComputer = req.body.contactQualityComputer;
+    const contactQualitySocial = req.body.contactQualitySocial;
+    const contactQualityDriver = req.body.contactQualityDriver;
 
-        app.post("/contactAdd", (req, res) => {
-            const contactName = req.body.contactName;
-            const contactResidence = req.body.contactResidence;
-            const contactDescription = req.body.contactDescription;
-            const contactPhoneNumber = req.body.contactPhoneNumber;
-            const contactQualityMedical = req.body.contactQualityMedical;
-            const contactQualityComputer = req.body.contactQualityComputer;
-            const contactQualitySocial = req.body.contactQualitySocial;
-            const contactQualityDriver = req.body.contactQualityDriver;
-
-            db.handleQuery(connectionPool, {
-                    query: "INSERT INTO contact (Name, Residence, TelephoneNr, canDrive, canMeet, Medical, Computer, Description) VALUES (?,?,?,?,?,?,?,?)",
-                    values: [contactName, contactResidence, contactPhoneNumber, contactQualityDriver, contactQualitySocial, contactQualityMedical, contactQualityComputer, contactDescription]
-                }, (data) => {
-                    //just give all data back as json
-                    res.status(httpOkCode).json(data);
-                }, (err) => res.status(badRequestCode).json({reason: err})
-            );
-        });
-
-        app.post("/contactChange", (req, res) => {
-            const contactName = req.body.contactName;
-            const contactResidence = req.body.contactResidence;
-            const contactDescription = req.body.contactDescription;
-            const contactPhoneNumber = req.body.contactPhoneNumber;
-            const contactQualityMedical = req.body.contactQualityMedical;
-            const contactQualityComputer = req.body.contactQualityComputer;
-            const contactQualitySocial = req.body.contactQualitySocial;
-            const contactQualityDriver = req.body.contactQualityDriver;
-
-            db.handleQuery(connectionPool, {
-                query:"UPDATE contact SET Name = contactName, Residence = contactResidence, TelephoneNr = contactPhoneNumber, canDrive = contactQualityDriver, canMeet = contactQualitySocial, Medical = contactQualityMedical, Computer = contactQualityComputer, Description = contactDescription",
-                    //"WHERE contactID = x",
-                    values: [contactName, contactResidence, contactPhoneNumber, contactQualityDriver, contactQualitySocial, contactQualityMedical, contactQualityComputer, contactDescription]
-                }, (data) => {
-                    //just give all data back as json
-                    res.status(httpOkCode).json(data);
-                }, (err) => res.status(badRequestCode).json({reason: err})
-            );
-        });
-
-
-    }, (err) => res.status(badRequestCode).json({reason: err})
-);
+    db.handleQuery(connectionPool, {
+            query: "UPDATE contact SET Name = contactName, Residence = contactResidence, TelephoneNr = contactPhoneNumber, canDrive = contactQualityDriver, canMeet = contactQualitySocial, Medical = contactQualityMedical, Computer = contactQualityComputer, Description = contactDescription",
+            //"WHERE contactID = x",
+            values: [contactName, contactResidence, contactPhoneNumber, contactQualityDriver, contactQualitySocial, contactQualityMedical, contactQualityComputer, contactDescription]
+        }, (data) => {
+            //just give all data back as json
+            res.status(httpOkCode).json(data);
+        }, (err) => res.status(badRequestCode).json({reason: err})
+    );
+});
 
 module.exports = app;
 
