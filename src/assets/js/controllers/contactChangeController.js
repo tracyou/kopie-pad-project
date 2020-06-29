22254af13f1a943d41a822784682cc31bff696e0
T.S.K. Owusu <tracy.owusu@hva.nl>
2020-06-29 02:31:23 +0200

    poging inladen contact bij wijzigen

--- src/assets/js/controllers/contactChangeController.js
+++ src/assets/js/controllers/contactChangeController.js
@@ -15,8 +15,32 @@ class contactChangeController {
 
         this.contactChangeView.find("#a").on("click", () => this.onCreateContact(event));
 
+        this.getContact();
         $(".content").empty().append(this.contactChangeView);
 
+    }
+
+    async getContact() {
+        const contactId = sessionStorage.getItem('contact');
+
+        try {
+            const contact = await this.contactLoadForChangeRepository.get(contactId);
+
+            contact.forEach((contact) =>{
+                this.contactChangeView.find("#exampleContact").val(`${contact.name}`);
+               this.contactChangeView.find("#exampleWoonplaats").val(`${contact.residence}`);
+                this.contactChangeView.find("#exampleOmschrijving").val(`${contact.description}`);
+                this.contactChangeView.find("#exampleTelefoonnummer").val(`${contact.telephoneNr}`);
+                // $("#exampleCheck1").is(':checked') ? 1 : 0 = ;
+                // const contactQualityComputer = $("#exampleCheck2").is(':checked') ? 1 : 0;
+                // const contactQualitySocial = $("#exampleCheck3").is(':checked') ? 1 : 0;
+                // const contactQualityDriver = $("#exampleCheck4").is(':checked') ? 1 : 0;
+            });
+
+        } catch (e) {
+            console.log("er is een error");
+            console.log(e);
+        }
     }
 
     async onCreateContact(event) {
@@ -24,55 +48,53 @@ class contactChangeController {
 
         const contactId = sessionStorage.getItem('contact');
 
-        try{
+        try {
             const data = await this.contactLoadForChangeRepository.get(contactId);
 
-            var userId;
-            const id = await this.userRepository.get(sessionManager.get("username"));
-            const firstReplace = JSON.stringify(id).replace(/\[\{\"id\"\:/, " ");
-            userId = firstReplace.replace(/\}\]/, " ");
+            console.log(data);
 
-        const contactName = this.contactChangeView.find("#exampleContact").val();
-        const contactResidence = this.contactChangeView.find("#exampleWoonplaats").val();
-        const contactDescription = this.contactChangeView.find("#exampleOmschrijving").val();
-        const contactPhoneNumber = this.contactChangeView.find("#exampleTelefoonnummer").val();
-        const contactQualityMedical = $("#exampleCheck1").is(':checked') ? 1:0 ;
-        const contactQualityComputer = $("#exampleCheck2").is(':checked') ? 1:0 ;
-        const contactQualitySocial = $("#exampleCheck3").is(':checked') ? 1:0 ;
-        const contactQualityDriver = $("#exampleCheck4").is(':checked') ? 1:0 ;
+            // const contactName = this.contactChangeView.find("#exampleContact").val();
+            this.contactChangeView.getElementById('exampleContact').value = data.name;
+            const contactResidence = this.contactChangeView.find("#exampleWoonplaats").val();
+            const contactDescription = this.contactChangeView.find("#exampleOmschrijving").val();
+            const contactPhoneNumber = this.contactChangeView.find("#exampleTelefoonnummer").val();
+            const contactQualityMedical = $("#exampleCheck1").is(':checked') ? 1 : 0;
+            const contactQualityComputer = $("#exampleCheck2").is(':checked') ? 1 : 0;
+            const contactQualitySocial = $("#exampleCheck3").is(':checked') ? 1 : 0;
+            const contactQualityDriver = $("#exampleCheck4").is(':checked') ? 1 : 0;
 
-        if (contactName.length === 0 || contactResidence.length === 0 ||
-            contactDescription.length === 0 || contactPhoneNumber.length === 0) {
-            alert('U heeft niet alle velden ingevuld!');
-        } else {
-            console.log(contactName);
-            console.log(contactResidence);
-            console.log(contactDescription);
-            console.log(contactPhoneNumber);
+            if (contactName.length === 0 || contactResidence.length === 0 ||
+                contactDescription.length === 0 || contactPhoneNumber.length === 0) {
+                alert('U heeft niet alle velden ingevuld!');
+            } else {
+                console.log(contactName);
+                console.log(contactResidence);
+                console.log(contactDescription);
+                console.log(contactPhoneNumber);
 
-            console.log(contactQualityMedical);
-            console.log(contactQualityComputer);
-            console.log(contactQualitySocial);
-            console.log(contactQualityDriver);
+                console.log(contactQualityMedical);
+                console.log(contactQualityComputer);
+                console.log(contactQualitySocial);
+                console.log(contactQualityDriver);
 
-            console.log(contactId);
-            console.log(userId);
+                console.log(contactId);
+                console.log(userId);
 
-            try {
-                await this.contactChangeRepository.change(contactName, contactResidence, contactDescription,
-                    contactPhoneNumber, contactQualityMedical, contactQualityComputer, contactQualitySocial,
-                    contactQualityDriver, contactId, userId);
-                app.loadController(CONTROLLER_CONTACTEN);
-            } catch (e) {
-                if (e.code === 401) {
-                    this.contactChange
-                        .find(".error")
-                        .html(e.reason);
-                } else {
-                    console.log(e);
+                try {
+                    await this.contactChangeRepository.change(contactName, contactResidence, contactDescription,
+                        contactPhoneNumber, contactQualityMedical, contactQualityComputer, contactQualitySocial,
+                        contactQualityDriver, contactId, userId);
+                    app.loadController(CONTROLLER_CONTACTEN);
+                } catch (e) {
+                    if (e.code === 401) {
+                        this.contactChange
+                            .find(".error")
+                            .html(e.reason);
+                    } else {
+                        console.log(e);
+                    }
                 }
             }
-        }
         } catch (e) {
             if (e.code === 401) {
                 this.contactChange
