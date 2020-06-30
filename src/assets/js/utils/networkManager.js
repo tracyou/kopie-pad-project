f9811f71d0941fd1599ab52584b160d54596749b
TM van Breukelen <tessa.van.breukelen@hva.nl>
2020-06-30 13:21:14 +0200

    Klaarmaken HBO cloud

--- src/assets/js/utils/networkManager.js
+++ src/assets/js/utils/networkManager.js
@@ -14,10 +14,6 @@ class NetworkManager {
     doRequest(route, data = {}) {
         const json = JSON.stringify(data);
 
-        //TODO: for requests on same host - server is always same host - validate if its best solution
-        const serverPort = 3000;
-        const baseUrl = `${location.protocol}//${location.hostname}:${serverPort}`;
-
         const url = baseUrl + route;
 
         console.log(`Doing request to ${url}\nJSON: ${json}`);
